d5fd89eb4bb83177f3fd0fc18a8783b7f6e8baca
Guram Gognidze <quram21@gmail.com>
2025-11-15 15:40:49 +0400

    Add constructor and getters for class Task

--- Task.js
+++ Task.js
@@ -1,9 +1,40 @@
 export class Task {
+    static #idNumber = 0;
+
     #id;
     #name;
     #description;
     #creationDate;
     #isComplete;
+
+    constructor(name, description){
+        this.#name = name;
+        this.#description = description;
+        this.#creationDate = new Date();
+        this.#isComplete = false;
+        this.#id = Task.#idNumber++;
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    get description(){
+        return this.#description;
+    }
+
+    get creationDate(){
+        return this.#creationDate;
+    }
+
+    get isComplete(){
+        return this.#isComplete;
+    }
+
 
     editTask(){
 
--- TaskManager.js
+++ TaskManager.js
@@ -12,4 +12,8 @@ export class TaskManager {
     sortByNameAndDate(){
 
     }
+
+    deleteTask(task){
+
+    }
 }
