20c6daa25b73340584ab85f85bf4d7f6576c7ba3
Guram Gognidze <quram21@gmail.com>
2025-11-15 23:19:07 +0400

    Added button which hide 'add task' form

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 import { Task } from "./Task.js";
 import { TaskManager } from "./TaskManager.js";
 
-console.log("Hello JavaScript!");
+const hideFormButton = document.getElementById("hide-form");
+const addTaskForm = document.getElementById("add-task-form");
+
+addTaskForm.style.display = "none";
+
+hideFormButton.addEventListener("click", (e) => {
+    hideFormButton.style.transition = "1000ms";
+
+    if(addTaskForm.style.display === "none") {
+        addTaskForm.style.display = "flex";
+    }
+    else {
+        addTaskForm.style.display = "none";
+    }
+
+    if (hideFormButton.textContent === "add task"){
+        hideFormButton.textContent = "back";
+    }
+    else {
+        hideFormButton.textContent = "add task";
+    }
+})
